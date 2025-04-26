f50bb0376a656eb612c0efc7379289ebf021f03a
yusuf-masood <your_email@example.com>
2025-04-26 05:59:57 +0500

    Complete Project

--- Recipe-Finder-App-main/Final Project/backEnd/recipes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('recipes/', views.get_all_recipes, name='get_all_recipes'),
-    path('recipes/add/', views.add_recipe, name='add_recipe'),
-]
--- Recipe-Finder-App-main/Final Project/backEnd/shareRecipes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('recipes/', views.recipe_list_create, name='recipe-list-create'),
-    path('recipes/<int:pk>/', views.recipe_detail, name='recipe-detail'),
-]
